d2307055b2b31df77a43d55c40f206ee40aba7e6
umangraval <umangraval2000@gmail.com>
2021-03-20 18:23:45 +0530

    fix: remove auth & 404 route

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,6 +1,6 @@
 export default [
   {
-    path: "/homepage",
+    path: "/",
     name: "homepage",
     component: () => import("@/views/Homepage"),
     meta: {
@@ -20,7 +20,7 @@ export default [
     name: "car-details",
     component: () => import("@/views/CarDetails"),
     meta: {
-      requiresAuth: true,
+      // requiresAuth: true,
       title: "Car Details",
     },
   },
@@ -29,16 +29,16 @@ export default [
     name: "virtual-tour",
     component: () => import("@/views/VirtualTour"),
     meta: {
-      requiresAuth: true,
+      // requiresAuth: true,
       title: "Virtual Tour",
     },
   },
-  {
-    path: "/*",
-    name: "page-not-found",
-    component: () => import("@/views/PageNotFound"),
-    meta: {
-      title: "404 - Page not found",
-    },
-  },
+  // {
+  //   path: "/*",
+  //   name: "page-not-found",
+  //   component: () => import("@/views/PageNotFound"),
+  //   meta: {
+  //     title: "404 - Page not found",
+  //   },
+  // },
 ];
