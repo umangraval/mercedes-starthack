a6125437967d393b1d0d24ba4712703ff2c8ac41
umangraval <umangraval2000@gmail.com>
2021-03-20 22:06:12 +0530

    feat: engine specs

---
+++ frontend/src/views/EngineSpecs.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card
+    class="pl-3 pr-3 mt-0 pt-0 mb-0 pb-0 container"
+    flat
+    outlined
+    elevation="1"
+    max-width="750px"
+  >
+    <!-- <router-link :to="{ name: 'car-selection'}" key="car-selection">  -->
+    <v-icon size="25" color="white" @click="goBack()">mdi-arrow-left</v-icon
+    ><span class="white--text ml-1">back</span>
+    <!-- </router-link> -->
+    <h3 class="title">Engine Specs</h3>
+    <video class="mt-3" width="100%" height="400" controls>
+      <source src="../assets/videos/specs.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "EngineSpecs",
+
+  data: () => ({
+    goBack() {
+      return this.$router.go(-1);
+    }
+  })
+};
+</script>
+
+<style scoped>
+.container {
+  background: #000;
+}
+.title {
+  font-size: 1.5rem !important;
+  margin-bottom: 8px !important;
+  margin-top: 15px !important;
+  margin-left: 25px;
+  color: rgb(216, 216, 216);
+}
+</style>
