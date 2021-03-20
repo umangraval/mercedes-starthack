221ccaf305ed5e270294c59647808b3c2a998b20
umangraval <umangraval2000@gmail.com>
2021-03-20 21:32:22 +0530

    feat: virtual tour video & specs model

--- frontend/src/views/VirtualTour.vue
+++ frontend/src/views/VirtualTour.vue
@@ -1,47 +1,44 @@
 <template>
-
-  <v-card            
-    class="pl-3 pr-3 mt-0 pt-0 mb-0 pb-0 container"                        
+  <v-card
+    class="pl-3 pr-3 mt-0 pt-0 mb-0 pb-0 container"
     flat
-    outlined                       
+    outlined
     elevation="1"
     max-width="750px"
   >
-  <!-- <router-link :to="{ name: 'car-selection'}" key="car-selection">  -->
-      <v-icon size="25" color="white" @click="goBack()">mdi-arrow-left</v-icon><span class="white--text ml-1">back</span>
+    <!-- <router-link :to="{ name: 'car-selection'}" key="car-selection">  -->
+    <v-icon size="25" color="white" @click="goBack()">mdi-arrow-left</v-icon
+    ><span class="white--text ml-1">back</span>
     <!-- </router-link> -->
     <h3 class="title">Full Virtual Tour</h3>
-
+    <video class="mt-3" width="100%" height="400" controls>
+      <source src="../assets/videos/virtualtour.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   </v-card>
-  
 </template>
 
 <script>
-
 export default {
-  name: 'VirtualTour',
+  name: "VirtualTour",
 
   data: () => ({
     goBack() {
-        return this.$router.go(-1)
-      }
-  }),
-  
-}
+      return this.$router.go(-1);
+    }
+  })
+};
 </script>
 
 <style scoped>
-
-
-.container{
+.container {
   background: #000;
 }
-.title{
+.title {
   font-size: 1.5rem !important;
   margin-bottom: 8px !important;
   margin-top: 15px !important;
-  margin-left: 25px ;
-  color: rgb(216, 216, 216); 
+  margin-left: 25px;
+  color: rgb(216, 216, 216);
 }
-
 </style>
